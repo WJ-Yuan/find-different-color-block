2acb848d69db6afe8506f52509d690b446525158
davidyuan <ywjdavid@126.com>
2023-07-22 02:52:33 +0800

    refactor deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  build: {
-    outDir: 'docs',
-  },
   base: '/find-different-color-block/',
 });
